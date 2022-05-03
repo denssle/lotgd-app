4861b6f04fa35a48770232b3cc459a95b43890ed
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2022-05-03 13:32:42 +0200

    update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,7 +7,7 @@ import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
   html: SafeHtml;
 
   constructor(private httpServie: HttpService, private sanitizer: DomSanitizer) {
@@ -16,16 +16,15 @@ export class HomePage implements OnInit{
   ngOnInit(): void {
     this.httpServie.getStartPage();
 
-    this.httpServie.observeHTML().subscribe(value => {
-      if(value) {
-        console.log(value.indexOf('<!--'));
-        console.log(value.indexOf('<!--', 350));
-        this.html = this.sanitizer.bypassSecurityTrustHtml(value);
-        const el = document.createElement( 'html' );
-        el.innerHTML = value;
-        const html: HTMLCollection = el.getElementsByTagName('script');
-        const list = Array.from(html);
-        list.forEach(value1 => console.log(value1.innerHTML));
+    this.httpServie.observeHTML().subscribe((html: string) => {
+      if (html) {
+        console.log(html);
+        this.html = this.sanitizer.bypassSecurityTrustHtml(html);
+        const containerHTML: HTMLElement = document.createElement('html');
+        containerHTML.innerHTML = html;
+        const scriptCollection: HTMLCollection = containerHTML.getElementsByTagName('script');
+        const list = Array.from(scriptCollection);
+        list.forEach(script => console.log(script.innerHTML));
       }
     });
   }
