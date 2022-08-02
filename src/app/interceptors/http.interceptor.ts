76a45a7b5b1edaccfdec618e4372578f56301d0e
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2022-08-02 08:28:33 +0200

    debug update

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -1,22 +1,30 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {DebugService} from '../services/debug.service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-	constructor() {
-	}
+  constructor(private debug: DebugService) {
+  }
 
-	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		const baseUrl: string = document.getElementsByTagName('base')[0].href;
-    console.log('interceptor', baseUrl);
-		if (req.url.includes('lotgd.de')) {
-      return next.handle(req.clone({url: `${req.url}`}));
-		}
-    if(req.url.includes('templates')) {
-      console.log('interceptor image');
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const baseUrl: string = document.getElementsByTagName('base')[0].href;
+    if (req.url.includes('lotgd.de')) {
+      return this.handleResponse(next.handle(req.clone({url: `${req.url}`})));
     }
-    return next.handle(req.clone({url: `${baseUrl}${req.url}`}));
-	}
+    return this.handleResponse(next.handle(req.clone({url: `${baseUrl}${req.url}`})));
+  }
+
+  private handleResponse(source: Observable<HttpEvent<any>>): Observable<HttpEvent<any>> {
+    source.toPromise()
+      .then(value => {
+        this.debug.debug('HttpInterceptor: ' + JSON.stringify(value));
+      })
+      .catch(reason => {
+        this.debug.debug('HttpInterceptor Error: ' + JSON.stringify(reason));
+      });
+    return source;
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,16 @@ import {Injectable} from '@angular/core';
 import {HttpService} from './http.service';
 import {DebugService} from './debug.service';
 import {HTTPResponse} from '@awesome-cordova-plugins/http/ngx';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  authenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpService, private debug: DebugService) {
+  constructor(private http: HttpService, private debug: DebugService, private router: Router) {
   }
 
   public login(name: string, password: string) {
@@ -16,8 +19,10 @@ export class AuthService {
       .then(value => {
         this.debug.debug('SUCCESS of Auth');
         const response = value as HTTPResponse;
-        this.debug.debug('auth: header: ' + JSON.stringify(response.headers));
-        this.debug.debug('auth: data: ' + JSON.stringify(response.data));
+        // this.debug.debug('AUTH: header: ' + JSON.stringify(response.headers));
+        // this.debug.debug('AUTH: data: ' + JSON.stringify(response.data));
+        this.authenticated.next(true);
+        // this.router.navigate(['home']);
       })
       .catch(reason => {
         this.debug.debug('ERROR: ' + JSON.stringify(reason));
